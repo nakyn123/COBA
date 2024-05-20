7a711654042315fce8430b8cede00bdae1411728
nakyn123 <nakyn.nattawin@gmail.com>
2024-05-20 13:51:22 +0700

    tugas 1,2,3

---
+++ user W3/src/User.java
@@ -0,0 +1,50 @@
+class user {
+    protected String username;
+    protected String password;
+    protected String phoneNumber;
+    protected String email;
+    protected Boolean admin = false;
+    protected final String country = "Indonesia";
+
+    user(String username, String password, String phoneNumber, Boolean admin){
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.admin = admin;
+    }
+
+    user(String username, String password, Boolean admin){
+        this.username = username;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    user(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    user(String username){
+        this.username = username;
+    }
+
+    user(){
+    }
+
+    void userMe() throws Exception {
+        System.out.println("My username is "+this.username+ 
+        " password "+this.password+ 
+        " phone Number "+this.phoneNumber+ 
+        " email "+this.email+ 
+        (this.admin? " is_admin ": "")+
+        " country "+this.country);
+    }
+
+    void login(String username, String password){
+        if(this.username == username && this.password == password) {
+            System.out.println("Success login with username "+username);
+        }else{
+            System.out.println("Failed login with username "+username);
+        }
+    }
+}
